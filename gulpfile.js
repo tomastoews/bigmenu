2be72881423c6e2c1781d7c8dbe0606b69e51013
Tomas Töws <tomas.toews96@gmx.de>
2019-11-02 21:51:31 +0100

    Overworked, improved and extented gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,42 +1,44 @@
 const gulp =            require('gulp');
+const minifyHTML =      require('gulp-html-minifier2');
 const sass =            require('gulp-sass');
 const minifyCSS =       require('gulp-clean-css');
 const autoprefixer =    require('gulp-autoprefixer');
+const babel =           require('gulp-babel');
 const minifyJS =        require('gulp-uglify');
 const browserSync =     require('browser-sync').create();
 
-const gutil =           require('gulp-util');
-
 // Copy all HTML files
-gulp.task('copyHTML', () => {
-    gulp.src('src/*.html')
+const copyHTML = () => {
+    return gulp.src('src/*.html')
+        .pipe(minifyHTML({collapseWhitespace: true}))
         .pipe(gulp.dest('dist'));
-});
+};
+
+// Copy facicons folder
+const copyFavicons = () => {
+    return gulp.src('src/facicon/**/*.*')
+            .pipe(gulp.dest('dist/favicons'));
+};
 
 // Compile Sass & Minify CSS
-gulp.task('compileSASS', () => {
-    return gulp.src('./src/sass/**/*.scss')
+const compileSASS = () => {
+    return gulp.src('./src/sass/*.scss') // ./src/sass/**/*.scss
             .pipe(sass().on('error', sass.logError))
             .pipe(autoprefixer({
                 browsers: ['last 2 versions', 'not ie <= 8'],
                 cascade: false
             }))
             .pipe(minifyCSS())
-            .pipe(gulp.dest('./dist/css'));
-});
+            .pipe(gulp.dest('dist/css'));
+};
 
-// Copy all JS files
-gulp.task('copyJS',() => {
-    gulp.src('src/js/*.js')
-        .pipe(gulp.dest('dist/js'))
-});
-
-// Minify JS
-gulp.task('minifyJS', (cb) => {
-    gulp.src('dist/js/*.js')
-        .pipe(minifyJS().on('error', function (err) { gutil.log(gutil.colors.red('[Error]'), err.toString()); }))
-        .pipe(gulp.dest('dist/js'));
-});
+// Compile & Minify JS
+const compileJS = () => {
+    return gulp.src('src/js/*.js')
+            .pipe(babel({presets: ['es2015']}))
+            .pipe(minifyJS({compress:true}).on('error', function (err) { console.log('ERROR: ', err.toString()); }))
+            .pipe(gulp.dest('dist/js'));
+};
 
 // Browser Sync
 gulp.task('serve', () => {
@@ -50,3 +52,10 @@ gulp.task('watch', () => {
     gulp.watch('./src/sass/**/*.scss', ['compileSASS']);
     gulp.watch('./src/js/*.js', ['copyJS', 'minifyJS']);
 });
+
+exports.build = gulp.series(
+    copyHTML,
+    copyFavicons,
+    compileSASS,
+    compileJS
+);
